9318d2fd0af5c76fb8d74fdb5eedd8a856620d89
Lukasz Jarzynka <jarzyn18@gmail.com>
2022-07-12 16:29:44 +0200

    lekcja 7 Symfonycast

--- src/Controller/SymfonyController.php
+++ src/Controller/SymfonyController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class SymfonyController
+class SymfonyController extends AbstractController
 {
     #[Route('/')]
     public function homepage() : Response {
-        return new Response('dupa dupa dupa dupa dupa ');
+
+        $tracks = [
+            ['song' => 'Gdybys Kiedys','artist' => 'Paluch'],
+            ['song' =>' 6 Zer', 'artist' =>  'Taco Hemingway'],
+            ['song' => 'Marmur','artist' => 'Taco Hemingway'],
+            ['song' =>' Ladni Smutni Ludzie', 'artist' =>  'Paluch'],
+            ['song' => 'Cesarz','artist' => 'Keke'],
+            ['song' =>' Kabriolety', 'artist' =>  'Taco Hemingway'],
+            ['song' => 'Europa','artist' => 'Taco Hemingway'],
+        ];
+
+        return $this->render("symfony/homepage.html.twig",[
+           'title'=> 'Strona Glowna',
+            'tracks'=> $tracks,
+        ]);
     }
     #[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response {
