0fb4bffd87d325e8450d8d0c0280a52ed9ececc2
Lukasz Jarzynka <jarzyn18@gmail.com>
2022-07-19 21:46:07 +0200

    Lekcja 13 - dodawanie serwisu logowania

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,7 +2,27 @@
 
 namespace App\Controller;
 
-class SongController
-{
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class SongController extends AbstractController
+{
+    #[Route('/api/songs/{id<\d+>}', methods: ['GET'])]
+    public function getSong(int $id, LoggerInterface $logger): Response
+    {
+        $song = [
+            'id' => $id,
+            'name' => 'Gdybys kiedys',
+            'url' => 'https://www.youtube.com/watch?v=qSNV03we7Cg',
+        ];
+
+        $logger->info('Zwraca odpowiedz API dla utworu {song} ', [
+           'song' => $id,
+        ]);
+
+        return $this->json($song);
+    }
 }
